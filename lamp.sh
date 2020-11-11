b0f1c8a02687794ae672ed133f36bb08c723d73f
Jacob Fenton <greendayfanj@gmail.com>
2020-11-11 04:39:44 -0500

    LAMP Stack

---
+++ lamp.sh
@@ -0,0 +1,6 @@
+#####################################LAMP####################################
+
+sudo apt update
+sudo apt install -y apache2 mysql-server php libapache2-mod-php php-mysql
+
+sudo mysql_secure_installation
