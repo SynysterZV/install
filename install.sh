c7b0220f755d3f88bbbd2f8e02f470a4fbe0f2b5
Jacob Fenton <greendayfanj@gmail.com>
2020-11-11 15:49:33 -0500

    Added PT Install

--- install.sh
+++ install.sh
@@ -23,6 +23,10 @@ wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-
 echo "deb [arch=amd64] https://download.virtualbox.org/virtualbox/debian eoan contrib" | sudo tee /etc/apt/sources.list.d/vbox.list
 sudo apt install virtualbox-6.1
 
+#Packet Tracer
+sudo apt install libgl1-mesa-glx
+sudo dpkg -i pt.deb
+
 #MetaPackage
 sudo apt install -y wireshark ubuntu-restricted-extras gparted gimp timeshift brave-browser gnome-tweaks gnome-shell-extensions openjdk-11-jdk default-jre snap wine-stable winetricks ttf-mscorefonts-installer unrar zip unzip p7zip-full p7zip-rar rar
 
